10fcb2123bc51b33b28033b2256b96ee35ad2e2d
JustinMutinta <JustinMutinta@hotmail.com>
2022-04-07 21:42:32 -0500

    Got the Swagger File to work

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 //This is the main class that will run the whole application
 //The @SpringBootApplication means that this is a spring boot app and will run as such. Also, check out the pom file
 @SpringBootApplication
-@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/example/demo/topic/TopicController.java
+++ demo/src/main/java/com/example/demo/topic/TopicController.java
@@ -1,14 +1,9 @@
 package com.example.demo.topic;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //Controller class. This is the driving force of the app. Everything runs through here
 //@RestController allows us to make requests
